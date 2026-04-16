845b458d08aa0bb3b8bd1108cfb16880195a683c
Aryan Raj Singh <aryanraj.singh@newspace.co.in>
2026-04-16 17:13:58 +0530

    New architecture and UI

---
+++ scout_display/lib/features/common_page/presentation/screens/common_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CommonPage extends StatelessWidget {
+  const CommonPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.transparent,
+      body: SizedBox.expand(),
+    );
+  }
+}
