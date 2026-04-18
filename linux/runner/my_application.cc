64d5ae31d16e99b6855d6584a0bc48e603eb7998
Karthikeya Tummala <tummala.karthikeya@newspace.co.in>
2026-04-18 10:03:20 +0530

    Make the app full screen

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -16,7 +16,12 @@ G_DEFINE_TYPE(MyApplication, my_application, GTK_TYPE_APPLICATION)
 
 // Called when first Flutter frame received.
 static void first_frame_cb(MyApplication* self, FlView* view) {
-  gtk_widget_show(gtk_widget_get_toplevel(GTK_WIDGET(view)));
+  GtkWindow* window = GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(view)));
+
+  gtk_window_set_decorated(window, FALSE);
+  gtk_window_fullscreen(window);
+
+  gtk_widget_show(GTK_WIDGET(window));
 }
 
 // Implements GApplication::activate.
